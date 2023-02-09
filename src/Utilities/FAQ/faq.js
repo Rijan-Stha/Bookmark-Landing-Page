c755c21bec33a4d5a278788ffa85a2718df63635
Rijan-Stha <shrestharijan056@gmail.com>
2023-02-09 12:21:54 +0545

    make aaccordion

--- src/Utilities/FAQ/faq.js
+++ src/Utilities/FAQ/faq.js
@@ -1,2 +1,21 @@
 import '../../styles/faq.css';
 
+export function faq(){
+    const faq = document.createElement("section");
+    faq.classList.add('faq', 'flow');
+  
+    const faq__title = document.createElement("h1");
+    faq__title.classList.add("section__title", "section__title--faq");
+    faq__title.textContent = `faq`;
+  
+    const faq__info = document.createElement("p");
+    faq__info.textContent = "Our aim is to make it quick and easy for you \
+                                  your favourite websites. Your bookmarks sync between your\
+                                  devices so you can access then on the go."
+    faq__info.classList.add("section__info", "section__info--faq");
+  
+    faq.append(faq__title, 
+                faq__info, 
+                tab());
+    return faq;
+}
